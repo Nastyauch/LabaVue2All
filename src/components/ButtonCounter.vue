255fc7616f68d9971afdaa31cb0970dd4cef6c2d
nastya <nastya.uchaneva@gmail.com>
2019-06-19 11:38:42 +0300

    allVue

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h1>Связанные поля</h1>
+      <input :style="{zoom}" v-model="count">
+      <hr>
+      <input :style="{zoom}" v-model="count">
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: ''
+    }
+  },
+  props: {
+    zoom: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: red;
+}
+
+</style>
